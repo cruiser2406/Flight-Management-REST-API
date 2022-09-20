159988c51309232ee529f1dbb3713f9a65ec5bea
priyanka <bhamidipatipriyanka@gmail.com>
2022-09-20 20:56:06 +0530

    'f'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const routes = require('./routes/flight'); // import the routes
 const mongoose = require('mongoose')
-
+const port = process.env.PORT || 3000
 const app = express();
 
 app.use(express.json());
@@ -18,6 +18,6 @@ mongoose
   });
 app.use('/api', routes); //to use the routes
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(port, () => {
     console.log('Your app is listening on port ' + listener.address().port)
 })
